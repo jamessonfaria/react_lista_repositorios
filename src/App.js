c1974e97fde9c6fbd185f33c8b0ed89b8278d118
Jamesson Faria <jamessonjr@gmail.com>
2023-08-28 17:19:10 -0300

    adjust in main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import RoutesApp from "./routes";
+import React from "react";
+
+import Routes from "./routes";
+import GlobalStyle from "./styles/global";
 
 function App() {
-  return <RoutesApp />;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
